d94e0f9f3ebc6f00c3f1a1b4d40374356fab7184
WanglyLab <songminfang@outlook.com>
2026-06-26 16:57:23 +0800

    Update TAC_Fgf20_scDist.R

--- TAC_Fgf20_scDist.R
+++ TAC_Fgf20_scDist.R
@@ -1,20 +1,26 @@
 library(Seurat)
 library(scDist)
-library(dplyr)                
-library(miloR)                
-library(SingleCellExperiment) 
+library(dplyr)
 
 rm(list=ls())
 load('./seurat_combined.RData')
-my_meta <- seurat_obj@meta.data %>% select(Group,Sample,NewCellType) %>% filter(Group %in% c('TAC','TAC+SLNP@Fgf20'))
-my_norm <- GetAssayData(seurat_obj,assay ='RNA',slot = 'data')
-my_norm <- my_norm[,rownames(my_meta)]
-identical(rownames(my_meta),colnames(my_norm))
-TAC_Fgf20_diff <- scDist(normalized_counts = my_norm,
-                         meta.data = my_meta,
-                         fixed.effects = "Group",
-                         random.effects="Sample",
-                         d=20,
-                         clusters="NewCellType")
+
+my_meta <- seurat_obj@meta.data %>%
+  select(Group, Sample, NewCellType) %>%
+  filter(Group %in% c('TAC', 'TAC+SLNP@Fgf20'))
+
+my_norm <- GetAssayData(seurat_obj, assay = 'RNA', layer = 'data')
+my_norm <- my_norm[, rownames(my_meta)]
+my_meta <- my_meta[colnames(my_norm), ]
+
+TAC_Fgf20_diff <- scDist(
+  normalized_counts = my_norm,
+  meta.data = my_meta,
+  fixed.effects = "Group",
+  random.effects = "Sample",
+  d = 20,
+  clusters = "NewCellType"
+)
+
 save(TAC_Fgf20_diff, file = 'TAC_Fgf20_diff_scDist.RData')
 DistPlot(TAC_Fgf20_diff)
